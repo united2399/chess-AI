6fb60bdc93f4795bf480d4c12409dd4ba2661564
united2399 <115886821+united2399@users.noreply.github.com>
2024-01-14 11:32:45 +0100

    Update chess_ai.py

--- chess_ai.py
+++ chess_ai.py
@@ -1,1 +1,20 @@
+import chess
+import pettingzoo
 
+from pettingzoo.classic import chess_v6
+
+env = chess_v6.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        mask = observation["action_mask"]
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample(mask)
+
+    env.step(action)
+env.close()
